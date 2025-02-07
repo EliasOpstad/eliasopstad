74d36cd42c03a1a21a5e09ec6461c8d7426b1bd5
Elias Opstad <eops@hotmail.no>
2025-02-07 13:18:32 +0100

    Created the website for presenting myself

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,17 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class", // eller 'media' for å følge systeminnstillinger
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",       // Legg til flere farger om nødvendig
+        secondary: "var(--secondary)",
       },
     },
   },
+    
   plugins: [],
 } satisfies Config;
