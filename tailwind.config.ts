e723d93a13a32dca7fc1767af14a4b2deb865700
Elias Opstad <eops@hotmail.no>
2025-02-07 14:20:09 +0100

    Added styles in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{css,scss,sass,less,styl,stylus}",
 
   ],
   darkMode: "class", // eller 'media' for å følge systeminnstillinger
